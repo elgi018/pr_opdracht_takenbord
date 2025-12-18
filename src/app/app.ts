88ee34dc19aa726a25a144fd8572a28ee4b39cb3
Eljarreau Da Costa Gomez <elgi018@hotmail.com>
2025-12-18 09:42:24 +0100

    ADD: added loader component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { TaskBoardComponent } from './features/task-board/task-board.component';
+import { LoaderComponent } from './shared/loader/loader.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavBarComponent, TaskBoardComponent],
+  imports: [NavBarComponent, TaskBoardComponent, LoaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/shared/loader/loader.component.ts
@@ -0,0 +1,13 @@
+import { Component, inject } from '@angular/core';
+import { TaskStore } from '../../features/task-board/store/task.store';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-loader',
+  imports: [MatProgressSpinnerModule],
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.scss',
+})
+export class LoaderComponent {
+  readonly store = inject(TaskStore);
+}
